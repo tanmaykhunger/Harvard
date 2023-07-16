963502ae9c7a7d4ac335ad5ef356aaf8edaa39c6
Tanmay Khunger <117931543+tanmaykhunger@users.noreply.github.com>
2023-07-16 21:10:21 +0530

    3 hours

---
+++ Work/Compare.py
@@ -0,0 +1,8 @@
+x  = int(input("What's x?"))
+y  = int(input("What's y?"))
+
+if x == y:
+    print("X is equal to Y")
+
+else:
+    print("X is not equal to Y")
---
+++ Work/Grade.py
@@ -0,0 +1,19 @@
+Score = int(input("What is your score"))
+
+if Score >= 90:
+    print("Grade: A")
+
+elif Score >= 80:
+    print("Grade: B")
+
+elif Score >= 70:
+    print("Grade: C")
+
+elif Score >= 60:
+    print("Grade: D")
+
+elif Score >= 50:
+    print("Grade: E")
+
+else:
+    print("Grade: F")
